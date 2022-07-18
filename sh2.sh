30f0ccba35915516e509dbc0155cf6614cdc87a0
choichangjun <30309434+choichangjun@users.noreply.github.com>
2022-07-18 16:23:36 +0900

    Update sh2.sh

--- sh2.sh
+++ sh2.sh
@@ -3,10 +3,16 @@ opkg update
 opkg install ffmpeg ffserver usbutils kmod-usb2 kmod-video-uvc  python3-pyserial openssh-sftp-server openssh-keygen sshpass
 mkdir /root/usbcam
 
-sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffmpeg.sh /root/usbcam
-sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffserver.conf /root/usbcam
-sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffserver /etc/init.d/ffserver
-sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffmpeg_sh /etc/init.d/ffmpeg_sh
+#sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffmpeg.sh /root/usbcam
+#sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffserver.conf /root/usbcam
+#sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffserver /etc/init.d/ffserver
+#sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffmpeg_sh /etc/init.d/ffmpeg_sh
+
+mv ffmpeg.sh /root/usbcam/ffmpeg.sh
+mv ffserver /etc/init.d/ffserver
+mv ffserver.conf /root/usbcam/ffserver.conf
+mv ffmpeg_sh /etc/init.d/ffmpeg_sh
+
 chmod +x /etc/init.d/ffmpeg_sh /etc/init.d/ffserver
 chmod +x /root/usbcam/ffmpeg.sh /root/usbcam/ffserver.conf
 
