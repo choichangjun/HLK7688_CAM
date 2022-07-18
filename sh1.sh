c0ae06f3486017ee569d582954026070cd7ac41a
choichangjun <30309434+choichangjun@users.noreply.github.com>
2022-07-18 16:13:15 +0900

    Create sh1.sh

---
+++ sh1.sh
@@ -0,0 +1,8 @@
+uci set firewall.@zone[1].input='ACCEPT'
+uci set firewall.@zone[1].output='ACCEPT'
+/etc/init.d/firewall restart
+uci set network.lan.proto='dhcp'
+uci set network.lan.ipaddr='192.168.61.1'
+/etc/init.d/network restart
+uci set system.@system[0].timezone='KST-9'
+/etc/init.d/system restart
