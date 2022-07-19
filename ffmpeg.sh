447178151018f69b55b8b86de9ff68b0a5c6832c
choichangjun <30309434+choichangjun@users.noreply.github.com>
2022-07-19 15:56:21 +0900

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -3,7 +3,7 @@ prev_serial=`ps | grep 'ffmpeg -f' | grep -v 'grep' | awk '{print $1}'`
 
 if [[ -z $prev_serial ]]; then
 echo "start ffmpeg web streaming"
-/usr/bin/ffmpeg -f v4l2 -s 320x240 -r 24 -i /dev/video0 http://localhost:8888/camera.ffm
+/usr/bin/ffmpeg -f v4l2 -s 320x240 -r 30 -i /dev/video0 http://localhost:8888/camera.ffm
 else
 exit 0
 fi
@@ -13,7 +13,7 @@ mjpeg=`ps | grep 'ffmpeg -f v4l2 -s 320x240 -r -4 -i /dev/video0 http://localhos
 camera.ffm' | grep -v 'grep' | awk '{print $1}'`
 if [[ -z $mjpeg ]]; then
 echo "restart ffmpeg web streaming"
-/usr/bin/ffmpeg -f v4l2 -s 320x240 -r 24 -i /dev/video0 http://localhost:8888/camera.ffm
+/usr/bin/ffmpeg -f v4l2 -s 320x240 -r 30 -i /dev/video0 http://localhost:8888/camera.ffm
 fi
 sleep 2
 done
