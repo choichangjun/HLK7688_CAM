5e8a2b686e0fe16e0db78c6facb1f1f5d27a8360
choichangjun <30309434+choichangjun@users.noreply.github.com>
2022-07-20 14:41:21 +0900

    Update sh1.sh

--- sh1.sh
+++ sh1.sh
@@ -3,11 +3,7 @@ uci set firewall.@zone[1].output='ACCEPT'
 /etc/init.d/firewall restart
 uci set network.lan.proto='dhcp'
 uci set network.lan.ipaddr='192.168.61.1'
-uci set network.wan.proto='static'
-uci set network.wan.ipaddr='192.168.1.161'
-uci set network.wan.gateway='192.168.1.1'
-uci set network.wan.netmask='255.255.255.0'
-uci set network.wan.dnsnameservers='168.126.63.1'
+
 /etc/init.d/network restart
 uci set system.@system[0].timezone='KST-9'
 /etc/init.d/system restart
