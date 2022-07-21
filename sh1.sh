a8be3c5c39fa15d5a01ead651ee6a886af55d114
choichangjun <30309434+choichangjun@users.noreply.github.com>
2022-07-21 15:39:39 +0900

    Update sh1.sh

--- sh1.sh
+++ sh1.sh
@@ -1,9 +1,9 @@
 uci set firewall.@zone[1].input='ACCEPT'
 uci set firewall.@zone[1].output='ACCEPT'
-/etc/init.d/firewall restart
 uci set network.lan.proto='dhcp'
 uci set network.lan.ipaddr='192.168.61.1'
-
-/etc/init.d/network restart
 uci set system.@system[0].timezone='KST-9'
+uci commit
 /etc/init.d/system restart
+/etc/init.d/network restart
+
