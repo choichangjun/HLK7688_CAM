98451a46a0d2195050eb4f624e249b6717778c13
choichangjun <30309434+choichangjun@users.noreply.github.com>
2022-08-23 13:52:14 +0900

    Update sh3.sh

--- sh3.sh
+++ sh3.sh
@@ -1,5 +1,5 @@
 uci set network.wan.proto='static'
-uci set network.wan.ipaddr='192.168.1.161'
+uci set network.wan.ipaddr='192.168.1.11'
 uci set network.wan.gateway='192.168.1.1'
 uci set network.wan.netmask='255.255.255.0'
 uci set network.wan.dns='168.126.63.1'
