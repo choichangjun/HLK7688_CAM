8aa70585649a6801da8cf3c40086ba8dabc8350b
choichangjun <30309434+choichangjun@users.noreply.github.com>
2022-08-23 13:49:10 +0900

    Update sh2.sh

--- sh2.sh
+++ sh2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # opkg update
-opkg install ffmpeg ffserver usbutils kmod-usb2 kmod-video-uvc  python3-pyserial openssh-sftp-server openssh-keygen sshpass
+opkg install ffmpeg ffserver usbutils kmod-usb2 kmod-video-uvc   openssh-sftp-server openssh-keygen
 mkdir /root/usbcam
-
+# python3-pyserial  sshpass
 #sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffmpeg.sh /root/usbcam
 #sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffserver.conf /root/usbcam
 #sshpass -p "123" scp changjun@192.168.1.15:/home/changjun/HLK7688A/ffserver /etc/init.d/ffserver
